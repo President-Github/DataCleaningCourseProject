56c6de7eda2727c273f9f88974da0b95a1983c47
President-Github <56783983+President-Github@users.noreply.github.com>
2020-01-18 21:31:48 +1000

    Completed run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -53,5 +53,35 @@ for (i in seq(2,length(names_vector))){
 names_vector<-c("set.type", "subject.id","V1", names_vector)  #Add additional column names
 names(combined_data)<-names_vector        #Apply naming to combined table
 
-## 
-combined_data<inner_join(activity_label_mapping,combined_data)
+
+## Subset Data to only the means and standard deviations of measurements 
+cleaned_data<-inner_join(activity_label_mapping,combined_data)%>%
+  select(set.type, subject.id, V2, matches("^t.*-std.*|^t.*-mean.*"))%>%
+  rename(activity.desc=V2)
+
+
+## Replace column names with descriptive variables
+clean_names<-names(cleaned_data)
+for (i in seq(1, length(clean_names))) 
+  {
+  clean_names[i] = gsub("^t|\\(\\)-|\\(\\)","", clean_names[i]) #remove unnecessary characters
+  clean_names[i] = gsub("()","", clean_names[i]) #remove unnecessary characters
+  clean_names[i] = gsub("Acc","Acceleration", clean_names[i]) #Make acceleration explicit
+  clean_names[i] = gsub("-std","StandardDeviation", clean_names[i]) #Make acceleration explicit
+  clean_names[i] = gsub("Mag","Magnitude", clean_names[i]) #Make Magnitude explicit
+  clean_names[i] = gsub("-mean","Mean", clean_names[i]) #Maintain camel case
+}
+names(cleaned_data)<-clean_names
+
+## Save cleaned data to a csv
+write.csv(cleaned_data,"CourseProject/output/cleaned_data.csv")
+
+
+## Create tidied data set
+tidy_data<-select(cleaned_data, everything(), -set.type)%>%
+  group_by(subject.id, activity.desc)%>%
+  mutate_at(vars(-"activity.desc",-"subject.id"),mean)%>%
+  distinct()
+
+## Save tidied data to a csv
+write.csv(cleaned_data,"CourseProject/output/tidy_data.csv")
